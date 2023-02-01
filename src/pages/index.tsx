f3eb594fd35a3bd49d7627b0eb6d2bc0fe070754
Júlio Cézar <captaingamergroot@gmail.com>
2023-02-01 09:57:47 -0300

    Estrutura page product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,14 @@ import 'keen-slider/keen-slider.min.css'
 
 import Stripe from 'stripe'
 import { stripe } from '@/lib/stripe'
+import Link from 'next/link'
 
 interface HomeProps {
   products: {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -30,14 +31,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} alt="" width={520} height={480} />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} alt="" width={520} height={480} />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
