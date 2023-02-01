9a862748864fc2d480f1bb7bfe3f1b6af89247d6
Júlio Cézar <captaingamergroot@gmail.com>
2023-02-01 20:32:29 -0300

    Checkout Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -19,6 +19,7 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
@@ -33,6 +34,10 @@ export default function Product({ product }: ProductProps) {
     )
   }
 
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -45,7 +50,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -85,6 +90,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: 'BRL',
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1hour
